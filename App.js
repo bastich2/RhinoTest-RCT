35013879cf946c04cbf9fe66df69f9306f215dbf
Jaime Bustamante <jimibustamante@gmail.com>
2020-02-26 13:30:18 -0300

    Adding the player 2

--- App.js
+++ App.js
@@ -9,11 +9,23 @@ import {View, Text, StyleSheet} from 'react-native';
 // } from 'react-native/Libraries/NewAppScreen';
 import Player from './components/Player';
 
+// console.log(Player.setCredentials({"hello": "HI"}));
+const credentials = {
+  clientId: 'UIFD0-KngoGd18snqlHR2msiKv-zWFEHAtlDRSAAeTg',
+  accountId: '3cbe220d-b8e9-45a4-8c36-87a50d63ba58',
+  clientSecret: 'hc4YVU_f683pGHPaRrMB_UEkXvjFF2vnoh5ywVTHOt0',
+};
+// [RBSDKV2 setKeysWithClientId:@"UIFD0-KngoGd18snqlHR2msiKv-zWFEHAtlDRSAAeTg"
+//           clientSecret:@"hc4YVU_f683pGHPaRrMB_UEkXvjFF2vnoh5ywVTHOt0"
+//             accountId:@"3cbe220d-b8e9-45a4-8c36-87a50d63ba58"];
 const App: () => React$Node = () => {
+  const onCredentialsSetted = event => {
+    console.log({event});
+  };
   return (
-    <View style={styles.mainContainer}>
+    <View credentials={credentials} style={styles.mainContainer}>
       <Text>Hello!</Text>
-      <Player style={styles.player} />
+      <Player style={styles.player} onCredentialsSetted={onCredentialsSetted} />
     </View>
   );
 };
--- components/Player.js
+++ components/Player.js
@@ -1,3 +1,4 @@
 // import React from 'react';
 import {requireNativeComponent} from 'react-native';
+console.log({hello: "HI!!"});
 module.exports = requireNativeComponent('RbPlayer');
